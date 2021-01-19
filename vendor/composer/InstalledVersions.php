d21f84049595e3fc5fd445cae2efd3c394e0ac66
Anderson <dimovanderson@gmail.com>
2021-01-19 17:34:49 -0300

    Campo EscolaNomeMoeda ok

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -19,7 +19,7 @@ private static $installed = array (
     'aliases' => 
     array (
     ),
-    'reference' => '33fe5f85a6e1df1d1b5bc384f93fd631dd8c92a1',
+    'reference' => '9b561986fe540d980ede288bcbb0f10389dd97b2',
     'name' => 'laravel/laravel',
   ),
   'versions' => 
@@ -435,7 +435,7 @@ private static $installed = array (
       'aliases' => 
       array (
       ),
-      'reference' => '33fe5f85a6e1df1d1b5bc384f93fd631dd8c92a1',
+      'reference' => '9b561986fe540d980ede288bcbb0f10389dd97b2',
     ),
     'laravel/tinker' => 
     array (
@@ -727,8 +727,8 @@ private static $installed = array (
     array (
       'provided' => 
       array (
-        0 => '1.0',
-        1 => '1.0.0',
+        0 => '1.0.0',
+        1 => '1.0',
       ),
     ),
     'psr/simple-cache' => 
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,7 +6,7 @@
     'aliases' => 
     array (
     ),
-    'reference' => '33fe5f85a6e1df1d1b5bc384f93fd631dd8c92a1',
+    'reference' => '9b561986fe540d980ede288bcbb0f10389dd97b2',
     'name' => 'laravel/laravel',
   ),
   'versions' => 
@@ -422,7 +422,7 @@
       'aliases' => 
       array (
       ),
-      'reference' => '33fe5f85a6e1df1d1b5bc384f93fd631dd8c92a1',
+      'reference' => '9b561986fe540d980ede288bcbb0f10389dd97b2',
     ),
     'laravel/tinker' => 
     array (
@@ -714,8 +714,8 @@
     array (
       'provided' => 
       array (
-        0 => '1.0',
-        1 => '1.0.0',
+        0 => '1.0.0',
+        1 => '1.0',
       ),
     ),
     'psr/simple-cache' => 
