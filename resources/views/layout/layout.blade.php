1219c36307a432e419df086dc8968c0523fed368
Flavio Lima <nunes.lima.flavio@gmail.com>
2021-03-13 15:09:43 -0300

    Alter name section

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -270,7 +270,7 @@
 			</div>
 			
 			<!-- breadcrumb -->
-            @section('sidebar')
+            @section('breadcrumb')
                 <div class="row wrapper border-bottom white-bg page-heading">
                     <div class="col-lg-10">
                         <h2>Buttons</h2>
